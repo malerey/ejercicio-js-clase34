a1b136b7a73911a48f506dc86da3944441039783
Malena Rey <36973939+malerey@users.noreply.github.com>
2019-11-03 21:31:24 -0300

    Update clase34.js

--- clase34.js
+++ clase34.js
@@ -30,6 +30,7 @@ const producto2Cant = compraProd2 == "si" && prompt(`Cuantas unidades desea llev
 
 const total = (producto1Cant * precioProd1) + (producto2Cant * precioProd2);
 
+// muy buena idea este alert! 
 total == 0 && alert("No ha realizado ninguna compra");
 
 
@@ -45,16 +46,29 @@ TOTAL
 total a abonar: $ ${total}`);
 
 
+
 const tarjCred = total != 0 && prompt(`💳 Desea abonar con tarjeta de credito?`);
 
 const cuotas = tarjCred == "si" && prompt(`Cantidad de cuotas:`) || 0;
 
 const valorCuota = total / cuotas;
 
+// esta condicion no funciona asi
+// valorCuota == Number nunca va a ser verdadero
+// en todo caso deberia ser 
+// typeof valorCuota == "number" 
+// En este caso, siempre evalua a "false"
+// No tiene consecuencias en el codigo porque no estamos haciendo nada con el valor
+// de esta operacion: js solo ve un "false" en el codigo y listo
+// si lo que queremos es chequear si valorCuota es un numero, y si no lo es, 
+// asignarle el valor 0, deberia ser una asignacion, asi:
+// valorCuota = typeof(valorCuota) == "number" || 0
+
 valorCuota == Number || 0;
 
 const cuotaConDesc = (total - (total * descuento)) / cuotas;
 
+// misma observacion que antes 
 cuotaConDesc == Number || 0;
 
 
@@ -77,9 +91,17 @@ const codigo = prompt(`cuenta con un codigo de descuento?`);
 
 const codigoIngresado = codigo == "si" && prompt(`Por favor, ingrese el codigo`);
 
+// esto no funciona, porque si el codigo es distinto a adalovelace, lo que se retorna
+// es un string "codigo incorrecto" y no el alerta "codigo incorrecto"
+// codigoIngresado == "adalovelace" && alert(`Codigo aceptado`) || alert(`Codigo incorrecto`);
 codigoIngresado == "adalovelace" && alert(`Codigo aceptado`) || `Codigo incorrecto`;
 
 
+// este alerta se va a mostrar aunque el codigo ingresado sea incorrecto
+// deberiamos mostrar solo el subtotal si el descuento es aceptado, por ejemplo 
+// haciendo la siguiente variable
+// codigoAceptado = codigoIngresado == "adalovelace" || false
+// despues en el alert, solo hacemos el descuento si codigoAceptado == true
 alert(`${cliente}, el detalle final de su compra es el siguiente 🛒:
 
 SUBTOTAL
